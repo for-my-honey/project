dad907bcdbd08ff30ca4c93175daa5bf51e788c5
for-my-honey <1255044799@qq.com>
2019-08-23 16:32:20 +0800

    local

---
+++ src/components/home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <homeHeader></homeHeader>
+    </div>
+</template>
+
+<script>
+import homeHeader from '@/components/homeHeader'
+export default {
+  name: 'home',
+  components:{
+      homeHeader
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/components/homeHeader.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <header>
+            <div class="header-left"><span class="iconfont">&#xe625;</span></div>
+            <div class="header-input"><span class="iconfont">&#xe632;</span>
+                <Input type="text" v-model="value" placeholder="搜索景点酒店"/>
+            </div> 
+            <div class="header-right">城市<span class="iconfont">&#xe62d;</span></div>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'homeHeader',
+  data(){
+      return {
+          value:''
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+@import '../assets/style.stylus'
+*
+    font-size :0.28rem
+header
+    height :.86rem
+    background :$bgColor
+    border :1px solid #17c0c8
+    display :flex
+    line-height :0.86rem
+    justify-content :space-between
+    .header-left,.header-right
+        margin :0 0.2rem
+    .header-input
+        background :white
+        border-radius 0.05rem
+        height :0.6rem
+        
+        line-height 0.6rem
+        margin :0.13rem 0
+        padding :0 0.1rem
+        input
+            border :none
+        input:visited
+            outline :none
+            border :1px solid white
+</style>
